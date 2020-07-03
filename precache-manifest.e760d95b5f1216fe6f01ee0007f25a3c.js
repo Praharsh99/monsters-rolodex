4b5edeb446ac3dc194d4014b59f7c8843dbce89d
Praharsh Paravastu <paravastupraharsh99@gmail.com>
2020-07-04 02:04:30 +0530

    Updates

---
+++ precache-manifest.e760d95b5f1216fe6f01ee0007f25a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4b6341a83a99593190b355ef22cb29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "267c7b5fbdfccfa4ec62",
+    "url": "/monsters-rolodex/static/css/main.d4df4b1d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267c7b5fbdfccfa4ec62",
+    "url": "/monsters-rolodex/static/js/main.1744a008.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
